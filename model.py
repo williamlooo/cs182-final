c15644cc9b178ec5c85a0c7bf96dea671e644932
William Loo <williamloo888@gmail.com>
2021-05-10 15:53:33 -0700

    implement novel loss, fix bug with labels, different unet implementation

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+from torchvision import models
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torch import nn
@@ -19,7 +20,8 @@ class DoubleConv(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5)
         )
 
     def forward(self, x):
@@ -73,6 +75,8 @@ class Net(nn.Module):
     def __init__(self, n_classes, im_height, im_width):
         super(Net, self).__init__()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.base_model = models.resnet18(pretrained=True)
+        self.base_layers = list(base_model.children()) 
         self.n_channels = 3
         self.n_classes = n_classes
 
@@ -108,3 +112,92 @@ class Net(nn.Module):
         x = torch.flatten(x, 1)
         logits = self.classifier(x)
         return logits
+
+
+def convreludropout(in_channels, out_channels, kernel, padding):
+    """
+    convolution->relu->dropout
+    """
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=padding),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.5),
+    )
+    
+#unet incorporating resnet weights
+#bilinear interpolation: https://arxiv.org/pdf/1805.09233.pdf
+#pytorch parts from: https://github.com/usuyama/pytorch-unet/
+class ResNetUNet(nn.Module):
+
+    def __init__(self, n_classes):
+        super().__init__()
+        self.base_model = models.resnet18(pretrained=True)
+        self.base_layers = list(self.base_model.children())                
+        
+        self.layer0 = nn.Sequential(*self.base_layers[:3]) # size=(N, 64, x.H/2, x.W/2)
+        self.layer0_1x1 = convreludropout(64, 64, 1, 0)
+        self.layer1 = nn.Sequential(*self.base_layers[3:5]) # size=(N, 64, x.H/4, x.W/4)        
+        self.layer1_1x1 = convreludropout(64, 64, 1, 0)       
+        self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)        
+        self.layer2_1x1 = convreludropout(128, 128, 1, 0)  
+        self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)        
+        self.layer3_1x1 = convreludropout(256, 256, 1, 0)  
+        self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+        self.layer4_1x1 = convreludropout(512, 512, 1, 0)  
+        
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        
+        self.conv_up3 = convreludropout(256 + 512, 512, 3, 1)
+        self.conv_up2 = convreludropout(128 + 512, 256, 3, 1)
+        self.conv_up1 = convreludropout(64 + 256, 256, 3, 1)
+        self.conv_up0 = convreludropout(64 + 256, 128, 3, 1)
+        
+        self.conv_original_size0 = convreludropout(3, 64, 3, 1)
+        self.conv_original_size1 = convreludropout(64, 64, 3, 1)
+        self.conv_original_size2 = convreludropout(64 + 128, 64, 3, 1)
+        
+        self.conv_last = OutConv(64, n_classes)
+
+        self.classifier = nn.Linear(in_features=819200, out_features=n_classes)
+
+        
+    def forward(self, input):
+        x_original = self.conv_original_size0(input)
+        x_original = self.conv_original_size1(x_original)
+        
+        layer0 = self.layer0(input)            
+        layer1 = self.layer1(layer0)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)        
+        layer4 = self.layer4(layer3)
+        
+        layer4 = self.layer4_1x1(layer4)
+        x = self.upsample(layer4)
+        layer3 = self.layer3_1x1(layer3)
+        x = torch.cat([x, layer3], dim=1)
+        x = self.conv_up3(x)
+ 
+        x = self.upsample(x)
+        layer2 = self.layer2_1x1(layer2)
+        x = torch.cat([x, layer2], dim=1)
+        x = self.conv_up2(x)
+
+        x = self.upsample(x)
+        layer1 = self.layer1_1x1(layer1)
+        x = torch.cat([x, layer1], dim=1)
+        x = self.conv_up1(x)
+
+        x = self.upsample(x)
+        layer0 = self.layer0_1x1(layer0)
+        x = torch.cat([x, layer0], dim=1)
+        x = self.conv_up0(x)
+        
+        x = self.upsample(x)
+        x = torch.cat([x, x_original], dim=1)
+        x = self.conv_original_size2(x)        
+        x = self.conv_last(x)   
+        x = torch.flatten(x, 1)
+        #print("x output shape: {}".format(x.shape))
+        out = self.classifier(x)
+        #print(out.shape)
+        return out
--- train_sample_torch.py
+++ train_sample_torch.py
@@ -16,6 +16,11 @@ from torch import nn
 import torch.nn.functional as F
 import os
 
+import pickle
+
+#nlp component
+import spacy
+
 import evaluation_tools
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
@@ -32,10 +37,16 @@ def main():
     print('Training on {} classes'.format(len(CLASS_NAMES)))
 
     # Create the training data generator
-    batch_size = 128
+    batch_size = 32
     im_height = 64
     im_width = 64
-    num_epochs = 10
+    num_epochs = 20
+    #training plan: 
+    #10 epochs at bs 4: train loss: 0.9852, val loss: 3.7852, train acc: 74.8540, val acc: 30.2800
+    #20 epochs at bs 16
+    #20 epochs at bs 32
+    #20 epochs at bs 64
+    #20 epochs at bs 128
 
     train_data_transforms = transforms.Compose([
         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
@@ -53,12 +64,32 @@ def main():
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                                shuffle=True, num_workers=4, pin_memory=True)
     print(f"train set size: {len(train_set)}")
+
+    #index to class id map
+    index_to_class_dict = {v: k for k, v in train_loader.dataset.class_to_idx.items()}
+    with open('index_to_class_dict.p', 'wb') as f:
+        pickle.dump(index_to_class_dict, f)
+
     val_set = torchvision.datasets.ImageFolder(data_dir / 'val-fixed', val_data_transforms)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=200,
                                                shuffle=True, num_workers=4, pin_memory=True)
     print(f"val set size: {len(val_set)}")
     train_losses = []
     val_losses = []
+
+    #load word dict
+    class_id_to_word_dict = {}
+    file = open('./data/tiny-imagenet-200/words.txt', 'r')
+    for line in file.readlines():
+        parts = line.strip().split('\t')
+        assert len(parts) == 2
+        class_id_to_word_dict[parts[0]] = parts[1]
+    print("word dict loaded.")
+
+    #nlp
+    nlp = spacy.load('en_core_web_md')
+    #!python -m spacy download en_core_web_md
+    print("nlp loaded.")
 
     def validate(val_loader, model, criterion, device):
         model.eval() #switch to eval mode
@@ -72,7 +103,8 @@ def main():
 
             # Forward pass and record loss
             y_hat = model(X)
-            loss = criterion(y_hat, y_true)
+            loss = criterion(y_hat, y_true, use_nlp=True)
+
             running_val_loss += loss.item() * X.size(0)
             _, predicted = y_hat.max(1)
             #print("\n")
@@ -85,14 +117,65 @@ def main():
         model.train() #switch back to train mode
         return model, val_loss, val_acc
 
+    def condense_label_groups(input_string):
+        condensed = []
+        for s in input_string:
+            synonyms = s.split(',')
+            #grabs default word (assuming everything in front are adjectives), very hacky, not ideal
+            picked = synonyms[0].split(' ')[-1].strip() 
+            for synonym in synonyms:
+                #chooses first word that contains no spaces and is within the nlp vocabulary
+                synonym = synonym.strip() #get rid of extra spacing around the word
+                if not ' ' in synonym and not nlp(synonym)[0].is_oov:
+                    picked = synonym
+                    break
+            condensed.append(picked)
+        return condensed
+
+    def CES_loss_function(outputs, targets, use_nlp=True, CE_weight=0.8, similarity_weight=0.2):
+        """
+        Cross-Entropy-Similarity loss function incorporating cross entropy loss with label similarity
+        """
+        CE_loss = nn.CrossEntropyLoss()(outputs, targets)
+
+        label_similarity_loss = 0
+        if use_nlp:
+            #grab predictions
+            probs = F.softmax(outputs, dim=1)
+            _, predicted = probs.max(1)
+            predicted = predicted.cpu().numpy()
+            ground_truth = targets.cpu().numpy()
+            #grab labels
+            predicted_labels = [class_id_to_word_dict[index_to_class_dict[index]] for index in predicted]
+            ground_truth_labels = [class_id_to_word_dict[index_to_class_dict[index]] for index in ground_truth]
+            #condense labels for nlp
+            condensed_predicted_labels = condense_label_groups(predicted_labels)
+            condensed_ground_truth_labels = condense_label_groups(ground_truth_labels)
+            #print(predicted_labels,"\n")
+            #print(ground_truth_labels,"\n")
+            #print(condensed_predicted_labels,"\n")
+            #print(condensed_ground_truth_labels,"\n")
+
+            #take averaged similarity score among all words, np.clip is used because of floating point imprecisions
+            similarities = [np.clip(nlp(condensed_predicted_labels[i])[0].similarity(nlp(condensed_ground_truth_labels[i])[0]),0,1) for i in range(len(condensed_ground_truth_labels))]
+            #print(similarities)
+            mean_similarity_score = np.mean(similarities)
+            #print(mean_similarity_score)
+
+            label_similarity_loss = 1-mean_similarity_score
+            #print(label_similarity_loss)
+
+        loss = (CE_weight*CE_loss) + (similarity_weight*label_similarity_loss)
+        return loss
+
     # Create the model
     #model = Net(len(CLASS_NAMES), im_height, im_width).to(device)
     model = ResNetUNet(len(CLASS_NAMES)).to(device)
     model.train()
     optim = torch.optim.Adam(model.parameters(),lr=1e-4, weight_decay=1e-5)
-    criterion = nn.CrossEntropyLoss()
-    path = "./weights/latest_9.pt"
-    #model.load_state_dict(torch.load(path), strict=True)
+    criterion = CES_loss_function
+    path = "./weights/latest_6.pt"
+    model.load_state_dict(torch.load(path), strict=True)
 
     for e in range(num_epochs):
         running_train_loss = 0
@@ -112,7 +195,7 @@ def main():
             #print(targets.shape)
             #print(f"\noutputs: {outputs}")
             #print(f"\ntargets: {targets}")
-            loss = criterion(outputs,targets)
+            loss = criterion(outputs,targets, use_nlp=True)
             
             loss.backward()
             optim.step()
@@ -120,7 +203,7 @@ def main():
             running_train_loss += loss.item() * inputs.size(0)
 
             _, predicted = outputs.max(1)
-            if e == 5 or e == 9:
+            if e == 9 or e == 19:
                 print(f"\npredicted label: {predicted}")
                 print(f"\ntarget labels: {targets}")
             
